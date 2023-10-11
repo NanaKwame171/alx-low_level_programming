2722e417244c03304112f00319d791c650871086
NanaKwame171 <nkay171@gmail.com>
2023-10-10 17:13:26 -0700

    My islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,11 @@
-int _islower(int c)
-{
-  return (c >= 'a' && c <= 'z');
+int main() {
+  char c = 'a';
+
+  if (_islower(c)) {
+    printf("%c is a lowercase character.\n", c);
+  } else {
+    printf("%c is not a lowercase character.\n", c);
+  }
+
+  return 0;
 }
