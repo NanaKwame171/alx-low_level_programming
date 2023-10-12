24d3f0efa29c650e1092cbce99c6ec2b5d65bd3b
NanaKwame171 <nkay171@gmail.com>
2023-10-12 15:57:10 -0700

    My isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,14 @@
-#include <stdio.h>
+#include "main.h"
 
-int _isupper(int c);
+/**
+ * _isupper - function that checks for uppercase characters
+ * @c: int type number
+ * Return: 1 if uppercase, else 0
+ */
+int _isupper(int c)
+{
+  if (c >= 'A' && c <= 'Z')
+    return (1);
 
-int main(void) {
-  char c;
-
-  c = 'A';
-  printf("%c: %d\n", c, _isupper(c));
-  c = 'a';
-  printf("%c: %d\n", c, _isupper(c));
-
-  return 0;
-}
-
-int _isupper(int c) {
-  return (c >= 'A' && c <= 'Z');
+  return (0);
 }
