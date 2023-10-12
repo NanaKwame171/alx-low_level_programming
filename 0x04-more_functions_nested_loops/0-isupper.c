81ecd4a7039a3a3fde88272b4aeb7bb7b2749c44
NanaKwame171 <nkay171@gmail.com>
2023-10-12 13:30:56 -0700

    My supper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+int _isupper(int c)
+{
+  if (c >= 'A' && c <= 'Z')
+    {
+      return 1;  // It is an uppercase character.
+    }
+  else
+    {
+      return 0;  // It is not an uppercase character.
+    }
+}
