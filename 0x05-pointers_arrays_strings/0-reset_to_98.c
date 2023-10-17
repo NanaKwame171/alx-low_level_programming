037cf1cf5d79f005afeab06e371e5dfc23c6d246
NanaKwame171 <nkay171@gmail.com>
2023-10-16 18:24:33 -0700

    My reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,40 @@
 #include "main.h"
+#include <stdio.h>
+#include <limits.h>
 
+/**
+ * reset_to_98 - Updates the value an integer pointer points to, to 98
+ * @n: Pointer to an integer
+ */
 void reset_to_98(int *n)
 {
   *n = 98;
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+  int n;
+
+  n = 402;
+  printf("Correct output: n = %d\n", n);
+
+  reset_to_98(&n);
+
+  printf("Correct output: n = %d\n", n);
+
+  n = -4096;
+  printf("Correct output: n = %d\n", n);
+
+  n = INT_MAX;
+  printf("Correct output: n = INT_MAX / n = %d\n", n);
+
+  n = INT_MIN;
+  printf("Correct output: n = INT_MIN / n = %d\n", n);
+
+  return (0); // Return SUCCESS
 }
